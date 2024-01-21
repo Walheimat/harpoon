762ecf5abe213f66eae51299b1ce96fc4615cc38
Walheimat <krister.schuchardt@gmail.com>
2024-01-21 19:43:06 +0100

    release: v0.3.4

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 PACKAGE_NAME=harpoon
-CURRENT_PACKAGE_VERSION=0.3.3
+CURRENT_PACKAGE_VERSION=0.3.4
 
 include dinghy/emacs-package.mk
--- harpoon.el
+++ harpoon.el
@@ -2,7 +2,7 @@
 
 ;; Author: Krister Schuchardt <krister.schuchardt@gmail.com>
 ;; Homepage: https://github.com/Walheimat/partial-recall
-;; Version: 0.3.3
+;; Version: 0.3.4
 
 ;; Keywords: frames files convenience
 
